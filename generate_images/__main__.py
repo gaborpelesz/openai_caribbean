5bd54f5b54a07efa5e08bb509dde431c2c32cf0d
gaborpelesz <gaborpelesz@hotmail.com>
2019-11-26 23:20:08 +0100

    Replace faulty image generation engine

--- generate_images/__main__.py
+++ generate_images/__main__.py
@@ -6,6 +6,29 @@ import time
 import argparse
 from generate_images import generate_images
 
+def get_URLS(is_test=False):
+    URLS_BASES = [
+        "stac/colombia/borde_rural",
+        "stac/colombia/borde_soacha",
+        "stac/guatemala/mixco_1_and_ebenezer",
+        "stac/guatemala/mixco_3",
+        "stac/st_lucia/dennery"
+    ]
+
+    ortho_extension = "{}_ortho-cog.tif"
+    labels_extension = ""
+    if not is_test:
+        labels_extension = "train-{}.geojson"
+    else:
+        labels_extension = "test-{}.geojson"
+
+    URLS = [(
+        base + "/" + ortho_extension.format(base.split("/")[-1]),
+        base + "/" + labels_extension.format(base.split("/")[-1])
+    ) for base in URLS_BASES]
+    return URLS
+
+
 def validate_urls(urls):
     urls = np.array(urls).flatten()
     for url in urls:
@@ -16,7 +39,21 @@ def validate_urls(urls):
     return True
 
 
+def create_label_folders(target):
+    folders_to_create = [
+        os.path.join(target, 'concrete_cement'),
+        os.path.join(target, 'healthy_metal'),
+        os.path.join(target, 'incomplete'),
+        os.path.join(target, 'irregular_metal'),
+        os.path.join(target, 'other')
+    ]
+    for f in folders_to_create:
+        if not os.path.exists(f):
+            os.makedirs(f)
+
+
 def main():
+    ### ARGUMENT PARSER ###
     parser = argparse.ArgumentParser(description='Choose wether to generate the training or the test images')
     parser.add_argument(
         '-t', '--test', 
@@ -24,52 +61,32 @@ def main():
         help='Switch to test images')
     args = parser.parse_args()
 
-    GENERATE_TRAINING = args.test
-    GENERATE_TEST = not GENERATE_TRAINING
-
-    process_start_time = time.time()
-
-    URLS_BASES = [
-        "stac/colombia/borde_rural",
-        "stac/colombia/borde_soacha",
-        "stac/guatemala/mixco_1_and_ebenezer",
-        "stac/guatemala/mixco_3",
-        "stac/st_lucia/dennery"
-    ]
+    ### TARGET DIRECTORY ###
+    GENERATE_TEST = args.test
+    GENERATE_TRAINING = not GENERATE_TEST
 
     target_dir = ""
     if GENERATE_TRAINING:
-        target_dir = "stac/training_data"
+        target_dir = "stac/datasets/training_data"
     if GENERATE_TEST:
-        target_dir = "stac/test_data"
-
-    ortho_extension = "{}_ortho-cog.tif"
-    # uncomment and comment above for debug and testing
-    # ortho_extension = "{}_ortho-cog-thumbnail.png"
-
-    map_extension = "{}-imagery.json"
-    labels_extension = ""
-    if GENERATE_TRAINING:
-        labels_extension = "training-{}.geojson"
-    if GENERATE_TEST:
-        labels_extension = "test-{}.geojson"
-    img_format = ortho_extension.split('.')[-1]
-
-    URLS = [(
-        base + "/" + ortho_extension.format(base.split("/")[-1]),
-        base + "/" + map_extension.format(base.split("/")[-1]),
-        base + "/" + labels_extension.format(base.split("/")[-1])
-    ) for base in URLS_BASES]
-
-    # if a file does not exists, exit program
-    if not validate_urls(URLS):
-        return
+        target_dir = "stac/datasets/test_data"
 
     if not os.path.exists(target_dir):
         os.makedirs(target_dir)
 
-    for url in URLS:
-        generate_images(url, to_folder=target_dir, img_format=img_format, test_data=GENERATE_TEST)
+    ### CREATE LABEL DIRECTORIES ###
+    create_label_folders(target_dir)
+
+    ### CALCULATE RUNTIME ###
+    process_start_time = time.time()
+
+    URLS = get_URLS(GENERATE_TEST)
+    # if a file does not exists, exit program
+    if not validate_urls(URLS):
+        return
+
+    for tif_path,geojson_path in URLS:
+        generate_images( (tif_path,geojson_path), to_folder=target_dir, test_data=GENERATE_TEST)
         gc.collect()
 
     print("Finished in: {:.3f}s".format(time.time()-process_start_time))
--- generate_images/generate_images.py
+++ generate_images/generate_images.py
@@ -1,109 +1,32 @@
 import numpy as np
-import cv2
-import json
+import rasterio
 import os
-from bigimage import Bigimage, Reader
-from preprocessing.perspective_transform import get_rotated_bounding_rect, four_point_transform
-from preprocessing.image_resize import image_resize
+import cv2
+from rasterio.mask import mask
+from rasterio.plot import show, reshape_as_image
+import geopandas as gpd
 
+def generate_images(urls, to_folder, test_data=False):
+    fpath_tiff = urls[0]
+    fpath_geojson = urls[1]
+    
+    print("Reading in: {} tif image".format(fpath_tiff.split("/")[-2]))
+    with rasterio.open(fpath_tiff) as tiff:
+            df_roof_geometries = gpd.read_file(fpath_geojson)
 
-def load_json_file(url):
-    with open(url) as f:
-        datastring = f.read()
+            tiff_crs = tiff.crs.data
+            df_roof_geometries['projected_geometry'] = (
+                df_roof_geometries['geometry'].to_crs(tiff_crs)
+            )
 
-    return json.loads(datastring)
+            roof_geometries = (
+                df_roof_geometries[['id', 'roof_material', 'projected_geometry']].values
+            )
+            numof_data = len(roof_geometries)
+            for i, roof in enumerate(roof_geometries):
+                roof_id, roof_material, projected_geometry = roof
+                print("Generating {} -> {} / {}".format(fpath_tiff.split("/")[-2], i+1, numof_data))
 
-
-def generate_images(image_url_tuple, to_folder, img_format="tif", test_data=False):
-    """
-    Params:
-        image_url_tuple: URL tuple, where tuples are path to the
-            following file -> (image.<tif || png>, image.json, roof_roofs.json)
-    """
-    image_url, image_json_url, roof_json_url = image_url_tuple
-
-    # load image bounding box
-    image_json = load_json_file(image_json_url)
-    image_bbox = [image_json["bbox"][:2], image_json["bbox"][2:]]
-
-    # load roof roof data
-    roofs_data = load_json_file(roof_json_url)
-
-    # read image
-    image = None
-    if img_format == "tiff" or img_format == "tif":
-        image = Reader.read_tif(image_url)
-    elif img_format == "png" or img_format == "jpg":
-        image = Reader.read_img(image_url)
-
-    # creating bigimage object
-    bigimage = Bigimage(image, image_bbox)
-
-    # saving length so we can print where we are
-    len_features_list = len(roofs_data["features"])
-
-    for i, feature in enumerate(roofs_data["features"]):
-        print(
-            "{} -> ({} / {})".format(image_url.split("/")[-2], i+1, len_features_list))
-        # we don't deal with multipolygons yet
-        if feature["geometry"]["type"] == "Polygon":
-            # access raw coordinates from json data
-            raw_roof_coordinates = feature["geometry"]["coordinates"][0]
-            # access roof ID and roof material
-            roof_id = feature["properties"]["id"]
-            roof_material = "" if test_data else feature["properties"]["roof_material"]
-
-            roof_coordinate_pixels = bigimage.coords_to_pixels(
-                raw_roof_coordinates)
-
-            roof_pixels = get_rotated_bounding_rect(
-                roof_coordinate_pixels, INFO=False)
-
-            roof_top_left = tuple(np.min(roof_pixels, axis=0).astype(
-                np.int32).tolist())
-            roof_bottom_right = tuple(np.max(roof_pixels, axis=0).astype(
-                np.int32).tolist())
-
-            # defines the additional size of the rectangle cut from the image
-            # the roof is inside that rectangle
-            rect_size = 20
-
-            # cut a rectangular shape from the image where the roof is in the middle
-            decreased_image = image_cut_rectangle(image, ([roof_top_left[0]-rect_size, roof_top_left[1]-rect_size],
-                                                          [roof_bottom_right[0]+rect_size, roof_bottom_right[1]+rect_size]))
-
-            # refactor roof pixels to match the decreased
-            roof_pixels[:, 0] = roof_pixels[:, 0] - \
-                (roof_top_left[0]-rect_size)
-            roof_pixels[:, 1] = roof_pixels[:, 1] - \
-                (roof_top_left[1]-rect_size)
-
-            # 2d perspective transform
-            transformed_roof_image = four_point_transform(
-                decreased_image, roof_pixels)
-
-            # scale image to 224x224
-            # transformed_roof_image = cv2.resize(
-            #    transformed_roof_image, (224, 224), interpolation=cv2.INTER_CUBIC)
-
-            # write image to destination
-            if roof_material != '':
-                destination_img = '{}/{}-{}.png'.format(to_folder, roof_id, roof_material)
-            else:
-                destination_img = '{}/{}.png'.format(to_folder, roof_id)
-            cv2.imwrite(destination_img, transformed_roof_image)
-
-
-def image_cut_rectangle(image, rectangle_vertices):
-    roof_top_left = list(rectangle_vertices[0])
-    roof_bottom_right = list(rectangle_vertices[1])
-    if roof_top_left[0] < 0:
-        roof_top_left[0] = 0
-    if roof_top_left[1] < 0:
-        roof_top_left[1] = 0
-    if roof_bottom_right[0] > image.shape[1]:
-        roof_bottom_right[0] = image.shape[1]
-    if roof_bottom_right[1] > image.shape[0]:
-        roof_bottom_right[1] = image.shape[0]
-
-    return image[roof_top_left[1]:roof_bottom_right[1], roof_top_left[0]:roof_bottom_right[0], :].copy()
+                img = mask(tiff, [projected_geometry], crop=True)[0]
+                target_path = os.path.join(to_folder, roof_material, str(roof_id)+'-'+roof_material+'.png')
+                cv2.imwrite(target_path, reshape_as_image(img))
