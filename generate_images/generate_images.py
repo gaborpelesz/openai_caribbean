965b7055e8087cede937c61e8faae8a7479d55cf
gaborpelesz <gaborpelesz@hotmail.com>
2019-11-26 23:24:08 +0100

    Implement test generating functionality

--- generate_images/generate_images.py
+++ generate_images/generate_images.py
@@ -19,14 +19,25 @@ def generate_images(urls, to_folder, test_data=False):
                 df_roof_geometries['geometry'].to_crs(tiff_crs)
             )
 
-            roof_geometries = (
-                df_roof_geometries[['id', 'roof_material', 'projected_geometry']].values
-            )
+            roof_geometries = ""
+            if not test_data:
+                roof_geometries = (
+                    df_roof_geometries[['id', 'roof_material', 'projected_geometry']].values
+                )
+            else:
+                roof_geometries = (
+                    df_roof_geometries[['id', 'projected_geometry']].values
+                )
             numof_data = len(roof_geometries)
             for i, roof in enumerate(roof_geometries):
-                roof_id, roof_material, projected_geometry = roof
                 print("Generating {} -> {} / {}".format(fpath_tiff.split("/")[-2], i+1, numof_data))
+                
+                if not test_data:
+                    roof_id, roof_material, projected_geometry = roof
+                    target_path = os.path.join(to_folder, roof_material, str(roof_id)+'-'+roof_material+'.png')
+                else:
+                    roof_id, projected_geometry = roof
+                    target_path = os.path.join(to_folder, str(roof_id)+'.png')
 
                 img = mask(tiff, [projected_geometry], crop=True)[0]
-                target_path = os.path.join(to_folder, roof_material, str(roof_id)+'-'+roof_material+'.png')
                 cv2.imwrite(target_path, reshape_as_image(img))
